dec5daf41ebe16828fd999c8dfe9520648c65611
Aakriti Aggarwal <56245613+aakriti1318@users.noreply.github.com>
2021-09-09 00:57:26 +0530

    Practice

---
+++ 01 Arrays/01 Reverse the array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int arr[], int n){
+    int s = 0, e = n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
+int main(){
+    int n; cin>>n; 
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    fun(arr,n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+    return 0;
+}
